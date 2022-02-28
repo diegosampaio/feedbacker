88443b70716118a05ebdcd1820b88cf5c962acd0
Diego Sampaio <diego.estaleiro@gmail.com>
2022-02-28 11:25:17 -0300

    first commit

---
+++ src/components/ModalLogin/index.vue
@@ -0,0 +1,24 @@
+<template>
+<h1>Modal de Login</h1>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  name: 'ModalLogin',
+  setup () {
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: '',
+        errorMessage: ''
+      },
+      password: {
+        value: '',
+        errorMessage: ''
+      }
+    })
+  }
+}
+</script>
---
+++ src/views/Credentials/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Aqui é a Credentials</h1>
+</template>
