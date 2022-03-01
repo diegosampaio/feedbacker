4534d1748a708b5279eff51c67b947f821c5b2f5
Diego Sampaio <diego.estaleiro@gmail.com>
2022-03-01 16:37:57 -0300

    armazenando dados do usuario no estado global

--- src/services/index.js
+++ src/services/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import router from '@/router'
 import AuthService from './auth'
 import UsersService from './users'
+import { setGlobalLoading } from '@/store/global'
 
 const API_ENVS = {
   production: '',
@@ -14,6 +15,7 @@ const httpClient = axios.create({
 })
 
 httpClient.interceptors.request.use(config => {
+  setGlobalLoading(true)
   const token = window.localStorage.getItem('token')
 
   if (token) {
@@ -22,16 +24,21 @@ httpClient.interceptors.request.use(config => {
   return config
 })
 
-httpClient.interceptors.response.use((response) => response, (error) => {
+httpClient.interceptors.response.use((response) => {
+  setGlobalLoading(false)
+  return response
+}, (error) => {
   const canThrowAnError = error.request.status === 0 || error.request.status === 500
 
   if (canThrowAnError) {
+    setGlobalLoading(false)
     throw new Error(error.message)
   }
 
   if (error.response.status === 401) {
     router.push({ name: 'Home' })
   }
+  setGlobalLoading(false)
   return error
 })
 
