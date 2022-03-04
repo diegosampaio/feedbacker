4c06f15bafd810f52a32e4fb575e54eeea8531b2
Diego Sampaio <diego.estaleiro@gmail.com>
2022-03-04 14:18:44 -0300

    Tela de credenciais

--- src/views/Credentials/index.vue
+++ src/views/Credentials/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Aqui é a Credentials</h1>
+  <div class="flex justify-center w-full bg-brand-main h-28">
+    <header-logged />
+  </div>
+  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">Credenciais</h1>
+    <p class="text-lg text-center text-gray-800 font-regular">
+      Guia de instalação e geração de suas credenciais.
+    </p>
+  </div>
 </template>
+<script>
+import HeaderLogged from '../../components/HeaderLogged'
+
+export default {
+  components: { HeaderLogged },
+  setup () {
+  }
+}
+</script>
